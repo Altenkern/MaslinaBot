f8bbd3f6b5645d1bbdde71a209a99c86ba1e9265
Altenkern <bednuy99@gmail.com>
2019-09-28 11:40:36 +0300

    before heroku

--- app.js
+++ app.js
@@ -60,8 +60,56 @@ bot.on("message", msg => {
     bot.sendMessage(msg.chat.id, ans);
   }
 });
+//купил нн шляпу
+bot.on("message", msg => {
+  if (
+    msg.text
+      .toString()
+      .toLowerCase()
+      .includes("купил") &&
+    msg.text
+      .toString()
+      .toLowerCase()
+      .includes("шляпу")
+  ) {
+    let ans = "А она ему как раз.";
+    console.log(msg.chat.title);
+    console.log(msg.text);
+    console.log(msg.from.username);
+    bot.sendMessage(msg.chat.id, ans);
+  }
+});
+
+//stipuha
+bot.on("message", msg => {
+  if (
+    msg.text
+      .toString()
+      .toLowerCase()
+      .includes("стипуха пришла")
+  ) {
+    let ans =
+      "Заходит в бар тот, кто побогател на 1300 гривен, а бармен ему говорит:\n - Андрей, пошёл нахуй!";
+    console.log(msg.chat.title);
+    console.log(msg.text);
+    console.log(msg.from.username);
+    bot.sendMessage(msg.chat.id, ans);
+  }
+});
+
 bot.onText(/\/spin/, msg => {
   path = spin.Spin();
   console.log(path);
   bot.sendPhoto(msg.chat.id, path);
+});
+
+bot.onText(/\/debug/, msg => {
+  bot.sendDocument(
+    msg.chat.id,
+    "http://freemap.com.ua/maps/genshtab/m-37-114.jpg"
+  );
+});
+
+bot.onText(/\/chmdeb/, msg => {
+  console.log("yoba");
 });
